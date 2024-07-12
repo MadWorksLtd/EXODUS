152db91fc78b47d966674ca90634a4f1f6f41ecb
Mad Works ltd <143761835+MadWorksLtd@users.noreply.github.com>
2024-07-12 16:34:44 +0600

    Add files via upload

--- hompage.js
+++ hompage.js
@@ -1,46 +1,50 @@
-const patternElement = document.getElementById("pattern");
-
-const countY = Math.ceil(patternElement.clientHeight / 40) + 1;
-const countX = Math.ceil(patternElement.clientWidth / 48) + 1;
-const gap = 2;
-
-for (let i = 0; i < countY; i++) {
-  for (let j = 0; j < countX; j++) {
-    const hexagon = document.createElement("div");
-    hexagon.style = `
-      background: url('data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iODciIGhlaWdodD0iMTAwIiB2aWV3Qm94PSIwIDAgODcgMTAwIiBmaWxsPSJub25lIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPgo8cGF0aCBkPSJNMi4xOTg3MyAyNi4xNTQ3TDQzLjUgMi4zMDk0TDg0LjgwMTMgMjYuMTU0N1Y3My44NDUzTDQzLjUgOTcuNjkwNkwyLjE5ODczIDczLjg0NTNWMjYuMTU0N1oiIGZpbGw9IiMxMzEyMTciIHN0cm9rZT0iIzEzMTIxNyIgc3Ryb2tlLXdpZHRoPSI0Ii8+Cjwvc3ZnPgo=') no-repeat;
-      width: 44px;
-      height: 50px;
-      background-size: contain;
-      position: absolute;
-      top: ${i * 40}px;
-      left: ${j * 48 + ((i * 24) % 48)}px;
-    `;
-
-    patternElement.appendChild(hexagon);
-  }
-}
-
-let mousePosition = {
-  x: 0,
-  y: 0
-};
-
-document.addEventListener("mousemove", (mouse) => {
-  mousePosition = {
-    x: mouse.clientX,
-    y: mouse.clientY
+window.onload = function () {
+  var e = document.querySelectorAll(".browse li"),
+    r = [];
+  removeActiveClasses = function (e) {
+    for (var r in e)
+      e[r].className = e[r].className.replace(/(?:^|\s)active(?!\S)/g, "");
   };
-});
-
-const loop = () => {
-  const gradientElement = document.getElementById("gradient");
-
-  gradientElement.style.transform = `translate(${mousePosition.x}px, ${mousePosition.y}px)`;
-
-  // Request the next animation frame
-  window.requestAnimationFrame(loop);
+  for (var a in e)
+    e.hasOwnProperty(a) &&
+      (e[a].addEventListener("click", function () {
+        event.preventDefault();
+      }),
+      e[a].addEventListener("mouseover", function () {
+        (r = [
+          document.querySelector(".browse__menu .active"),
+          document.querySelector(".browse__gallery .active")
+        ]),
+          removeActiveClasses(r),
+          (galleryToChange = document.querySelector(
+            ".browse__gallery ." + this.className
+          )),
+          (galleryToChange.className += " active"),
+          (this.className += " active");
+      }));
 };
-
-// Start the animation loop
-window.requestAnimationFrame(loop);
+window.onload = function () {
+  var e = document.querySelectorAll(".browse li"),
+    r = [];
+  removeActiveClasses = function (e) {
+    for (var r in e)
+      e[r].className = e[r].className.replace(/(?:^|\s)active(?!\S)/g, "");
+  };
+  for (var a in e)
+    e.hasOwnProperty(a) &&
+      (e[a].addEventListener("click", function () {
+        event.preventDefault();
+      }),
+      e[a].addEventListener("mouseover", function () {
+        (r = [
+          document.querySelector(".browse__menu .active"),
+          document.querySelector(".browse__gallery .active")
+        ]),
+          removeActiveClasses(r),
+          (galleryToChange = document.querySelector(
+            ".browse__gallery ." + this.className
+          )),
+          (galleryToChange.className += " active"),
+          (this.className += " active");
+      }));
+};
